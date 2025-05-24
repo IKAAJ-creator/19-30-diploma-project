0caff2801bd4ca345e6c4e85be346dc08d020250
IKAAJ-creator <nikitaporotnikov7@gmail.com>
2025-05-24 20:53:24 +0700

    zdandiplom.project

--- app.py
+++ app.py
@@ -8,40 +8,40 @@ def index():
 
 @app.route("/dogs")
 def dogs():
-    return render_template("dogs.html", title="Информация о собаках")
+    return render_template("dogs.html", title="dogs")
 
 @app.route("/cats")
 def cats():
-    return render_template("cats.html", title="Информация о котах")
+    return render_template("cats.html", title="cats")
 
 @app.route("/parrot")
 def parrot():
-    return render_template("parrot.html", title="Информация не о котах")
+    return render_template("parrot.html", title="parrots")
 
 @app.route("/rrabit")
 def rrabit():
-    return render_template("rrabit.html", title="Информация не о китайцах")
+    return render_template("rrabit.html", title="rabbits")
 
 @app.route("/Features")
 def Features():
-    return render_template("Features.html", title="Плюсы")
+    return render_template("Features.html", title="pluses")
 
 
 @app.route("/About")
 def About():
-    return render_template("About.html", title="Регестрация")
+    return render_template("About.html", title="registration")
 
 @app.route("/mem")
 def mem():
-    return render_template("mem.html", title="Пасхалочка1")
+    return render_template("mem.html", title="joke1")
 
 @app.route("/Andr")
 def Andr():
-    return render_template("Andr.html", title="Пасхалочка2")
+    return render_template("Andr.html", title="joke2")
 
 @app.route("/NNM")
 def NNM():
-    return render_template("NNM.html", title="Пасхалочка4")
+    return render_template("NNM.html", title="joke4")
 
 
 
